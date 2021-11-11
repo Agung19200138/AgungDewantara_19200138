78a6d1eb2a4123e2647390c8143cce9a6357c869
Agung19200138 <90890976+Agung19200138@users.noreply.github.com>
2021-11-11 13:10:59 +0700

    tugas wp 2 semester 3

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+33
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Agung</li>
+ <li>Nama Belakang : Dewantara</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Swadaya Limo Poncol</li>
+ </ul>
+
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepak Bola</li>
+ <li>Badminton</li>
+ </ul>
+</section>
